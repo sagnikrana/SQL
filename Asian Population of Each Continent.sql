2f7874812168c0b020aada4bd47030a758f7dc31
sagnikrana <sagnik.rana1994@gmail.com>
2020-03-08 19:46:58 -0400

    creating checkpoint

---
+++ Asian Population of Each Continent.sql
@@ -0,0 +1,11 @@
+/*Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) 
+and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.*/
+
+
+SELECT COUNTRY.CONTINENT,
+       ROUND(AVG(CITY.POPULATION) - 0.5) FROM CITY
+								         LEFT JOIN COUNTRY ON CITY.COUNTRYCODE = COUNTRY.CODE
+								         WHERE COUNTRY.CONTINENT IS NOT NULL
+								         GROUP BY COUNTRY.CONTINENT ;
